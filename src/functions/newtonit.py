0d04853b4b3a210078d6976e1ff93231742263e8
Matheus Vivas <matheusvivas.sfi@gmail.com>
2025-03-13 12:41:28 -0300

    controle de tensao em barras PV

--- src/functions/newtonit.py
+++ src/functions/newtonit.py
@@ -4,7 +4,17 @@ from numpy import linalg
 def newt_raph(barras,linhas,eps):
     ybus = linhas.ybus(barras)
     potencia = Scomplex(ybus,barras)
-    for i in range(19):
+    err = potencia.erros()
+    jac = potencia.jacob()
+    ijac = -linalg.inv(jac)
+    sol = []
+    for lin in ijac.tolist():
+        ss = 0
+        for k in range(len(lin)):
+            ss+= lin[k]*err[k]
+        sol.append(ss)
+    barras.updatebar(sol)
+    for ç in range(19):
         err = potencia.erros()
         if max(abs(var) for var in err) < eps: break
         jac = potencia.jacob()
@@ -16,5 +26,6 @@ def newt_raph(barras,linhas,eps):
                 ss+= lin[k]*err[k]
             sol.append(ss)
         barras.updatebar(sol)
-    barras.update2(potencia.pots())
+        barras.controlar()
+        barras.update2(potencia.pots())
     return barras
